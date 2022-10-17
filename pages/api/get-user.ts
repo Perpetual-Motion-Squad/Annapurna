a54c1e9f74adf58ace1adbfa6886748ca0b9e363
korigamik <korigamik@gmail.com>
2022-10-17 22:45:23 +0530

    add mongo db

---
+++ pages/api/get-user.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Users } from '../../src/db';
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method !== 'POST')
+        return res.status(405).send({ message: 'Only POST requests allowed' })
+
+    const { address } = req.body as { address: string };
+    const user = await Users.findOne({ address })
+    if (user)
+        res.status(200).json(user);
+    else
+        res.status(404).json({ message: "User not found" });
+}
