6ffe7848c029bb68cfb905fb0c91f42304519bb4
dhruv <dhruvbakshi12343@gmail.com>
2022-10-17 16:50:23 +0530

    navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,18 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
     return (
-        <div>
+        <div className="bg-black h-screen">
             <Head>
                 <title>Annapurna</title>
                 <meta name="description" content="Food for all" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div className="text-5xl font-nanum">
-                This is a sample text in reckless
-            </div>
+            <Navbar />
         </div>
     );
 };
