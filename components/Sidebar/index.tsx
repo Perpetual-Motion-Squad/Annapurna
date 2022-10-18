16a88d26c500aa6db87081e07bfeee10196a50e4
Monarch <92059211+Monarch47@users.noreply.github.com>
2022-10-18 23:59:53 +0530

    all routes added

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -8,7 +8,16 @@ const Sidebar = (props: Props) => {
   const SIZE = 30;
   return (
     <div className="flex flex-col border-r-[1px] border-[#d9d9d972] items-center gap-8 px-3 justify-between py-5 pt-10 sticky top-0 h-screen">
-      <Image src="/images/pin-point.svg" height={50} width={50} alt="logo" />
+      <Link href="/">
+        <div className="cursor-pointer">
+          <Image
+            src="/images/pin-point.svg"
+            height={50}
+            width={50}
+            alt="logo"
+          />
+        </div>
+      </Link>
       <div className="flex flex-col gap-10">
         <Link href="/participants">
           <div className="h-[50px] w-[50px] grid place-items-center rounded-full hover:bg-[#ffffff3a] hover:cursor-pointer transition-all dashboard__active">
