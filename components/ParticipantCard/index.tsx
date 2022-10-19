7674aa27f10cd817e10c9b85f9ba44901e73892f
korigamik <korigamik@gmail.com>
2022-10-19 13:29:10 +0530

    fix warning

--- components/ParticipantCard/index.tsx
+++ components/ParticipantCard/index.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 const ParticipantCard = (props: Props) => {
   return (
-    <a target="_blank" href={props.event.imageURL + '&' + new URLSearchParams({ username: props.username, userAddress: props.address })}>
+    <a target="_blank" rel="noreferrer" href={props.event.imageURL + '&' + new URLSearchParams({ username: props.username, userAddress: props.address })}>
       <div className="bg-[#383838] text-white w-full p-5 rounded-xl grid grid-cols-3 text-2xl gap-10 cursor-pointer text-center">
         <h1>{props.username}</h1>
         <h1 className="truncate">{props.address}</h1>
