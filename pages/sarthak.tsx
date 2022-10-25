20e93fac0ce83c365d867aa932f88b16489b9d29
Sart-Hack <sarthakgupta124@gmail.com>
2022-10-25 20:01:13 +0530

    Made page pretty and Add Navbar

--- pages/sarthak.tsx
+++ pages/sarthak.tsx
@@ -1,8 +1,17 @@
+import Navbar from 'components/Navbar'
 import React from 'react'
 
 const Sarthak = () => {
   return (
-    <div className = "bg-black text-white h-screen w-screen" >Sarthak god</div>
+    <div>
+        <Navbar />
+    <div className = "bg-black text-white h-screen w-screen flex justify-center items-center" >
+        
+        <h1 className='text-4xl font-sora font-semibold'>Sarthak god</h1>
+
+    </div>
+
+    </div>
   )
 }
 
